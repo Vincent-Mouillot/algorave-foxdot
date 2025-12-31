5808c173d2b97cea8a8d08bd6e309a1d19bdcda5
Vincent Mouillot <vmouillot@gmail.com>
2025-12-31 18:41:14 +0100

    Add the player in the drum pattern and call drums in test

--- patterns/drums.py
+++ patterns/drums.py
@@ -1,6 +1,7 @@
-def basic_drums():
-    p1 >> play("x-o-", dur=0.25)
+from FoxDot import *
 
-def broken_drums():
-    p1 >> play("x(o-)", dur=0.25)
+def basic_drums(d):
+    d >> play("x-o-", dur=0.25)
 
+def broken_drums(d):
+    d >> play("x(o-)", dur=0.25)
--- tests/2025-12-31_first_test.py
+++ tests/2025-12-31_first_test.py
@@ -1,11 +1,18 @@
+import patterns.drums as drums
+import importlib
+importlib.reload(drums)
+
+d1 = Player("d1")
+drums.basic_drums(d1)
+
 # --- tempo global ---
 Clock.bpm = 140
 
 # --- percussions simples ---
-p1 >> play("x-oo-x--[x,o]", dur=1/2)   # kick et snare
+p1 >> play("x-", dur=1/2, mix=0.3)   # kick et snare
 
 # --- basse simple ---
-b1 >> bass([0, 1, 3, 5], dur=1, lpf=600)
+b1 >> bass([0, 1, 3,[1,10]], dur=1, lpf=100)
 
 # --- mélodie légère ---
 p2 >> pluck([0, 4, 7, 12], dur=1/4, amp=0.5)
@@ -14,7 +21,9 @@ p2 >> pluck([0, 4, 7, 12], dur=1/4, amp=0.5)
 p1.room = 0.5
 p1.mix = 0.3
 b1.delay = 0.25
-p2.reverb = 0.2
-p1.stop()
-p2.stop()
-b1.stop()
+p1.reverb = 0.2
+
+# --- print ---
+print(Player.get_attributes())
+
+
